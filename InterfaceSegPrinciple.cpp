73e449da05e81de059dd404ec5bee1207050a358
Kaushik Sadashiva Reddy <kausredd@quicinc.com>
2022-04-08 15:41:53 -0700

    Create InterfaceSegPrinciple.cpp
    Interface Segregation Principle

---
+++ InterfaceSegPrinciple.cpp
@@ -0,0 +1,75 @@
+/* Interface Segregation Principle
+
+ - Do not have complex interfaces where we force the concrete classes to implement all the abstract methods
+ - Segregate the interfaces so that clients can only implement the interfaces that make sense for them
+
+*/
+
+#include <vector>
+struct Document;
+
+// This is a "polluted" or "fat" interface
+
+//struct IMachine
+//{
+//  virtual void print(Document& doc) = 0;
+//  virtual void fax(Document& doc) = 0;
+//  virtual void scan(Document& doc) = 0;
+//};
+//
+//struct MFP : IMachine
+//{
+//  void print(Document& doc) override;
+//  void fax(Document& doc) override;
+//  void scan(Document& doc) override;
+//};
+
+// 1. Recompile
+// 2. Client does not need this
+// 3. Forcing implementors to implement too much
+
+struct IPrinter
+{
+  virtual void print(Document& doc) = 0;
+};
+
+struct IScanner
+{
+  virtual void scan(Document& doc) = 0;
+};
+
+struct Printer : IPrinter
+{
+  void print(Document& doc) override;
+};
+
+struct Scanner : IScanner
+{
+  void scan(Document& doc) override;
+};
+
+struct IMachine: IPrinter, IScanner
+{
+};
+
+struct Machine : IMachine
+{
+  IPrinter& printer;
+  IScanner& scanner;
+
+  Machine(IPrinter& printer, IScanner& scanner)
+    : printer{printer},
+      scanner{scanner}
+  {
+  }
+
+  void print(Document& doc) override {
+    printer.print(doc);
+  }
+  void scan(Document& doc) override {
+    scanner.scan(doc);
+  }
+};
+
+// IPrinter --> Printer
+// everything --> Machine
